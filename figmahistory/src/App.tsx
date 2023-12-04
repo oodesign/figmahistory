000bf7fbf25df3f13e91d3ecec8af6ae569b727e
Oscar Oto <oscar.oto@dolby.com>
2023-12-04 07:06:56 +0100

    Retrieves all file history versions

--- figmahistory/src/App.tsx
+++ figmahistory/src/App.tsx
@@ -12,20 +12,55 @@ const Start = () => {
   const [popupWindow, setPopupWindow] = useState<Window | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  
-const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
 
-  async function figmaFileFetch(fileId:string) {
-    
+  const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
+
+  async function figmaFileFetch(fileId: string) {
+
+  }
+
+  async function fetchAllVersions(documentIDReceived: string, accessTokenReceived: string): Promise<any[]> {
+    const versions: any[] = [];
+
+    async function fetchPage(url: string | undefined): Promise<void> {
+      console.log("Fetching url: "+url);
+      if (url) {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${accessTokenReceived}` // Replace FigmaAPIKey with your actual access token
+          }
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+          versions.push(...data.versions);
+
+          // Continue fetching if there is a previous page
+          console.log("-- Has more pages? NextPage is:"+data.pagination.next_page)
+          if (data.pagination && data.pagination.next_page) {
+            await fetchPage(data.pagination.next_page);
+          }
+        } else {
+          console.error(`Failed to fetch versions: ${response.statusText}`);
+        }
+      }
+    }
+
+    // Start fetching with the initial page (undefined for the first page)
+    await fetchPage('https://api.figma.com/v1/files/' + documentIDReceived + "/versions");
+
+    return versions;
   }
 
   // Usage in a React component or elsewhere in your TypeScript code
-  const fetchFigmaFiles = async (accessTokenReceived: string) => {
+  const fetchFigmaFiles = async (documentIDReceived: string, accessTokenReceived: string) => {
 
-    console.log("Document ID is:"+documentID);
-    console.log("accessToken is:"+accessTokenReceived);
+    console.log("Document ID is:" + documentIDReceived);
+    console.log("accessToken is:" + accessTokenReceived);
 
-    let result = await fetch('https://api.figma.com/v1/files/' + documentID, {
+    let result = await fetch('https://api.figma.com/v1/files/' + documentIDReceived + "/versions", {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${accessTokenReceived}` // Replace FigmaAPIKey with your actual access token
@@ -33,12 +68,27 @@ const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
     })
 
     let figmaFileStruct = await result.json()
+
     console.log(figmaFileStruct);
+    const allVersions = await fetchAllVersions(documentIDReceived, accessTokenReceived);
+
+
+    console.log(allVersions);
+
+    let file1Id = figmaFileStruct.versions[0].id;
+    let file2Id = figmaFileStruct.versions[3].id;
+    console.log("Comparing " + file1Id + " vs. " + file2Id);
+
+
 
     return figmaFileStruct
   };
 
   const handleFigmaAuthentication = async (code: string) => {
+
+    let figmaDocumentID = getFigmaDocumentID();
+    console.log("Document IDs is:" + figmaDocumentID);
+    if (figmaDocumentID) setDocumentID(figmaDocumentID);
 
     console.log("Try post call");
     fetch('http://localhost:5002/get-figma-access-token', {
@@ -51,12 +101,13 @@ const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
       .then(async response => {
         const responseObject = await response.json();
         console.log(responseObject.figmaData)
-        console.log("AccessToken returned is:"+responseObject.figmaData.access_token)
-        if(responseObject.figmaData.access_token) setAccessToken(responseObject.figmaData.access_token);
-        fetchFigmaFiles(responseObject.figmaData.access_token)
+        console.log("AccessToken returned is:" + responseObject.figmaData.access_token)
+        if (responseObject.figmaData.access_token) setAccessToken(responseObject.figmaData.access_token);
+        fetchFigmaFiles(figmaDocumentID, responseObject.figmaData.access_token)
       })
       .then(data => console.log(data))
       .catch(error => console.error('Error:', error));
+
   };
 
   const getFigmaDocumentID = () => {
@@ -70,19 +121,16 @@ const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
     const matches = inputURL.match(regex)
 
     // if there are no matches, the user didn't paste a link to their figma file
-    if (matches === null) return
+    if (matches === null) return "";
 
     // if there is a match, the fourth group matched will be the file id
     const id = matches[4]
-    return id;
+    return id || "";
   }
 
 
   const openPopupWindow = () => {
 
-    let figmaDocumentID = getFigmaDocumentID();
-    console.log("Document ID is:" + figmaDocumentID);
-    if (figmaDocumentID) setDocumentID(figmaDocumentID);
 
     const url = 'https://www.figma.com/oauth?client_id=pLCXoLFHH1UngPRH0ENGzV&redirect_uri=http://127.0.0.1:5002/callFigmaOAuth&scope=file_read&state=qpolpolq&response_type=code'; // Replace with your actual URL
     const options = 'toolbar=no,\
@@ -98,6 +146,23 @@ const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
     setPopupWindow(popup);
   };
 
+  const getData = () => {
+    const token = "figu_aHdxX0k17O-wXPovNOqY85Tw-aFPpSQQrsxIoEiz";
+
+    let figmaDocumentID = getFigmaDocumentID();
+    console.log("Document IDs is:" + figmaDocumentID);
+    if (figmaDocumentID) setDocumentID(figmaDocumentID);
+
+    if (token && figmaDocumentID) {
+      console.log("Token is known")
+      setAccessToken(token);
+      fetchFigmaFiles(figmaDocumentID, token)
+    }
+    else {
+      openPopupWindow();
+    }
+  }
+
   useEffect(() => {
     const handlePopupData = (event: MessageEvent) => {
       // Check if the event data contains the parameters you expect
@@ -108,7 +173,7 @@ const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
         // Close the popup window
         if (popupWindow) {
           popupWindow.close();
-        } 
+        }
       }
     };
 
@@ -124,7 +189,7 @@ const FigmaAPIKey = "figd_RRFsYn0yPhRclt5nVvlfYPEdyazwfwlyPulZQBqc"
 
   return <div>
     <input id="figmaFileURL" type='text' placeholder='Paste your Figma URL here' defaultValue="https://www.figma.com/file/58J9lvktDn7tFZu16UDJHl/Dolby-pHRTF---Capture-app---No-Cloud?type=design&node-id=10163%3A65721&mode=design&t=6n0ZrLO9YyM2lHjb-1" />
-    <button onClick={openPopupWindow}>Auth Figma</button>
+    <button onClick={getData}>Auth Figma</button>
 
   </div>;
 };
